3a437294b877ec8dfa5e233d5bac8c9340c519eb
iosalger <mac@MactekiMac.local>
2013-10-31 18:20:25 +0800

    util

---
+++ BennyDriving/SourceCode/CoreCode/com.benny.network/UtilHttpRequestNetWork.h
@@ -0,0 +1,17 @@
+//
+//  UtilHttpRequestNetWork.h
+//  BennyDriving
+//
+//  Created by Alger on 13-10-31.
+//  Copyright (c) 2013年 Alger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ASIHttpHeaders.h" //ASIHttpRequest
+@interface UtilHttpRequestNetWork : NSObject
+
+
+
+
+
+@end
---
+++ BennyDriving/SourceCode/CoreCode/com.benny.network/UtilHttpRequestNetWork.m
@@ -0,0 +1,13 @@
+//
+//  UtilHttpRequestNetWork.m
+//  BennyDriving
+//
+//  Created by Alger on 13-10-31.
+//  Copyright (c) 2013年 Alger. All rights reserved.
+//
+
+#import "UtilHttpRequestNetWork.h"
+
+@implementation UtilHttpRequestNetWork
+
+@end
